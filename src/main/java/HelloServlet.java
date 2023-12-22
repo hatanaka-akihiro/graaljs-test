e8c876a1958f033933ab5e495a9449a83df95468
hatanaka.akihiro <hatanaka.akihiro@gmail.com>
2023-12-22 15:07:25 +0900

    GraalJS の初期化を行うように改良。

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -1,3 +1,4 @@
+import javax.script.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,7 +9,14 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.setContentType("text/html; charset=UTF-8");
-        resp.getWriter().println("Hello, world!");
+        resp.setContentType("text/plain; charset=UTF-8");
+
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("js");
+        if (engine == null) {
+            resp.getWriter().print("Failed to initialize GraalJS");
+        } else {
+            resp.getWriter().print("Hello GraalJS");
+        }
     }
 }
